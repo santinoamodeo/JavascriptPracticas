c0c7bf6d65a461e2f25d8e1de65fffdce8ba8daf
Santino Amodeo <santinoamodeoph@gmail.com>
2023-12-10 20:55:48 -0300

    hof 2

--- 8-15-11-23/JS/index.js
+++ 8-15-11-23/JS/index.js
@@ -88,3 +88,158 @@
 
 /////////////////////////
 
+//ceil, flour, round => redondear nros 
+
+//ceil =>  retorna el nro indicado redondeando arriba
+
+// let decimal = Math.ceil(1.45)
+// alert(decimal);
+
+
+//floor => lo mismo nada mas que hacia abajo
+
+// let decimal2 = Math.floor(1.45)
+// alert(decimal);
+
+//round => redondear al entero mas cercano
+
+// let round = Math.round(1.5)
+// alert(round)
+
+//////////////////////
+
+//metodos para raiz y potencia
+
+//raiz => sqrt
+// let raiz = Math.sqrt(9)
+
+// console.log(raiz)
+
+
+//potencia = pow(base, exponente)
+// let pow = pow(2,4);
+// console.log(pow)
+
+//////////////////////
+
+//metodo random => retorna un nro aleatorio entre
+//  y 0 y 1
+
+// let nroAleatorio = Math.random()
+// console.log(nroAleatorio)
+
+// console.log(Math.random()*10)
+// console.log(Math.round(Math.random()*10))
+
+//los nros entre 10 y 50
+
+// console.log(Math.round(Math.random()*40+10)) //arranca en 10
+
+
+//juego del dado
+
+// console.log(Math.round(Math.random() * 5 +1))
+
+
+////////CLASS DATE//////////////
+
+//date => para trabajar con fechas y horas
+
+let fechaActual = new Date();
+// console.log(fechaActual)
+
+//date info= ano, mes, dia, hora-min-seg
+
+// let especifica = new Date(2023,11,15,03,00,15) //declaro la fecha
+// console.log(especifica)
+
+// let anio = especifica.getFullYear()
+// console.log(anio)
+
+// let mes = especifica.getMonth()
+// console.log(mes)
+
+// let dia = especifica.getDate()
+// console.log(dia)
+
+// let  hora = especifica.getHours()
+// console.log(hora)
+
+
+//setear- establecer componentes de fecha y hora
+
+// fechaActual.setFullYear(2021)
+
+// console.log(fechaActual)
+
+////////////
+
+//pedido de datos toDate
+
+// console.log(fechaActual.toDateString())
+// console.log(fechaActual.toLocaleString())
+// console.log(fechaActual.toLocaleTimeString())
+
+////////////
+
+//alquilar peliculas
+
+class Cliente{
+    constructor(nombre, edad, membresia){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.membresia = membresia;
+    }
+}
+
+class Alquiler{
+    constructor(pelicula, cliente, costo, fecha){
+        this.pelicula = pelicula;
+        this.cliente = cliente;
+        this.costo = Math.round(costo);
+        this.fecha = fecha || new Date();
+    }
+}
+
+class Pelicula{
+    constructor(nombre, genero){
+        this.nombre = nombre;
+        this.genero = genero;
+    }
+}
+
+const cliente1 = new Cliente("emiliano", 20, "premium")
+const cliente2 = new Cliente('sebastian', 30, 'free')
+
+let pelicula1 = new Pelicula('50 sombras de grey', 'fantasia')
+let pelicula2 = new Pelicula('Fast & Furious 7', 'accion')
+
+let alquiler1 = new Alquiler(pelicula1, cliente1, 40.5)
+let alquiler2 = new Alquiler(pelicula2, cliente2, new Date("june 6, 2023 10:00"))
+
+function diasAlquilerMembresia(cliente){
+    if(cliente.membresia == 'premium'){
+        return 3;
+    }
+    else{
+        return 2;
+    }
+}
+
+function peliculaDevolverHoy(alquiler){
+    let fechaAlquiler = alquiler.fecha;
+    let fechaActual = new Date();
+    let diasAlquiler = diasAlquilerMembresia(alquiler.cliente);
+
+    fechaAlquiler.setDate(fechaAlquiler.getDate() + diasAlquiler)
+
+    if(fechaAlquiler < fechaActual){
+        return `la pelicula ${alquiler.pelicula.nombre} debe devolverse hoy, el costo del alquiller es de ${alquiler.pelicula.precio} dolares`
+    }
+    else{
+        return `la pelicula ${alquiler.pelicula.nombre} no debe devolverse hoy, el costo del alquiller es de ${alquiler.costo} dolares`
+    }
+}
+
+console.log(peliculaDevolverHoy(alquiler1))
+console.log(peliculaDevolverHoy(alquiler2))
