6d0b13543bd8ee4e1c87c35e7622aa059c3025a5
Santino Amodeo <santinoamodeoph@gmail.com>
2023-12-11 19:28:53 -0300

    dom y carrito zarpado

--- 9-DOM/js/index.js
+++ 9-DOM/js/index.js
@@ -53,19 +53,65 @@ TITULO.innerText = "estoy jugando soloaoaoaoa"
 
 const DIV_NUEVO = document.getElementById("divContenedor")
 DIV_NUEVO.innerHTML =
-`
-<div>
-<p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum doloremque sequi dolore dolores velit magni aut odio voluptates, commodi alias quam dicta possimus fugiat consectetur iure animi incidunt non voluptatum?
-</p>
-<ul>
-    <li>s</li>
-    <li>s</li>
-    <li>s</li>
-    <li>s</li>
-    <li>s</li>
-</ul>
-</div>
-`
+    `<div class="pepe">
+            <p>hola</p>
+    </div>`
 
-1.20
+
+/////
+
+//agregar nodos desde JS
+
+//createElement("") 
+
+//var = document.createElement(nombre del elemento)
+
+let parrafo = document.createElement("p")
+
+parrafo.textContent = "Hola estew parafo tyiene js ca;ooooksmmsmsms" 
+//le da el contenido
+
+document.body.appendChild(parrafo);
+//appemdChild agrega el nodo, hijo de body
+
+//////////////
+//objetos dinamicos
+
+class Producto{
+    constructor(nombre,precio,img){
+        this.nombre = nombre
+        this.precio = precio
+        this.img = img
+    }
+}
+
+
+const MOTHER = new Producto("Asus", 1200, "./assets/img/asus.jpg") //img toma la ruta como si estuviera en HTML
+const MOUSE = new Producto("Logi Silent", 200, "./assets/img/logi.jpg")
+const TECLADO = new Producto("Logitech", 800, "./assets/img/logitech.jpg")
+
+const ArrayProductos = [MOTHER, MOUSE, TECLADO];
+
+ArrayProductos.forEach(Producto=>{
+    let div = document.createElement("div");
+    div.className = "card";//aplica clases al div
+    // div.innerHTML = `   
+    //     <p>Nombre: ${Producto.nombre} </p>
+    //     <p>Precio: ${Producto.precio} </p>
+    //     <img src="${Producto.img}" alt="producto"> 
+    //     <button>Agregar a carrito</button>
+    // `
+    div.innerHTML = `
+    <div class="card" style="width: 18rem;">
+    <img src="${Producto.img}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${Producto.nombre}</h5>
+      <p class="card-text">${Producto.precio}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+    `
+
+    contenedorProductos.appendChild(div);
+})
+
